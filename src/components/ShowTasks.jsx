d9c11ecc74f9fa1d6ea6fdfcef0a79b985fae559
huseyin <huseyinarslan06@gmail.com>
2022-07-05 00:45:44 +0300

    third step

--- src/components/ShowTasks.jsx
+++ src/components/ShowTasks.jsx
@@ -1,5 +1,19 @@
-const ShowTasks = () => {
-  return <div>ShowTasks</div>;
+import { MdDeleteForever } from "react-icons/md";
+
+const ShowTasks = ({ tasks }) => {
+  return (
+    <div className="task">
+      {tasks.map((task) => {
+        return (
+          <div key={task.id}>
+            <h3>{task.task} </h3>
+            <h3>{task.day} </h3>
+            <MdDeleteForever />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ShowTasks;
