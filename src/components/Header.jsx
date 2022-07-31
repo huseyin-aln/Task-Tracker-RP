6243bf2e26527428b977f83b1953a475770ba505
huseyin <huseyinarslan06@gmail.com>
2022-07-31 15:18:30 +0300

    fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,6 @@ const Header = ({ tasks, setTasks }) => {
     bgColor: "purple",
   });
 
-  //! React, default olarak state'leri hemen degistirmeyebilir.
-  //! Ekstra render'lari azaltmak icin state'leri toplu (batch) bir sekilde gunceller.
-  //! Bir event handler icerisindeki ardasik state'ler event bitiminde toplu bir
-  //! sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gorere yapilir.
-  //! Ayni event icerisinde birbirine bagli state'leri kullanirken buna dikkat etmek gerkir.
-
   //? https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates
 
   const handleShow = () => {
--- src/components/ShowTasks.jsx
+++ src/components/ShowTasks.jsx
@@ -1,17 +1,40 @@
 import { MdDeleteForever } from "react-icons/md";
+import { useState } from "react";
 
-const ShowTasks = ({ tasks }) => {
+const ShowTasks = ({ tasks, setTasks }) => {
+  const [done, setDone] = useState(true);
+
+  const handleClick = (e) => {
+    if (done) {
+      e.target.classList.add("done");
+      setDone(!done);
+    } else {
+      e.target.classList.remove("done");
+      setDone(!done);
+    }
+  };
+
+  const handleDelete = (e) => {
+    setTasks(tasks.filter((item) => item.id !== e));
+  };
   return (
-    <div className="task">
-      {tasks.map((task) => {
-        return (
-          <div key={task.id}>
-            <h3>{task.task} </h3>
-            <h3>{task.day} </h3>
-            <MdDeleteForever />
-          </div>
-        );
-      })}
+    <div>
+      <div className="task" onClick={handleClick}>
+        {tasks?.map((item) => {
+          const { id, task, day } = item;
+          return (
+            <div key={id} className="task-div">
+              <h3>{task} </h3>
+              <h3>{day} </h3>
+              <MdDeleteForever
+                size={25}
+                color="red"
+                onClick={() => handleDelete(id)}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
